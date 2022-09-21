e92aed228843292e3043791297372302d6dadbb1
blugee <blugee>
2022-09-22 01:31:44 +0530

    add share popup or liveuser icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
-import Editor from './pages/Editor'
+import EditorWraped from './pages/EditorWraped';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/:id" element={<Editor />} />
+        <Route path="/:id" element={<EditorWraped />} />
         {/* <Route path="*" element={<Navigate to="/1" />}/> */}
       </Routes>
     </div>
---
+++ src/pages/EditorWraped/index.tsx
@@ -0,0 +1,77 @@
+import { Suspense } from "react";
+import Avatar from '../../components/Avatar/Avatar';
+import { COLORS_PRESENCE } from '../../constants';
+import { RoomProvider, useMap, useMyPresence, useOthers } from "../../liveblocks.config";
+import Editor from '../Editor';
+import { LiveMap } from "@liveblocks/client";
+
+
+function WhoIsHere({ userCount }: any) {
+
+    return (
+        <div className="who_is_here"> {userCount} Live </div>
+    );
+}
+
+function Room() {
+
+    const others = useOthers();
+
+    return (
+        <div
+            className="container"
+        // onPointerMove={onCanvasPointerMove}
+        // onPointerUp={onCanvasPointerUp}
+        >
+            <div className='flex mt-2 ml-1.5 justify-end'>
+                <div className='flex'>
+                    {others.map(({ connectionId, presence }) => {
+                        if (!connectionId) {
+                            return null;
+                        }
+
+                        return (
+                            <Avatar
+                                key={connectionId}
+                                color={COLORS_PRESENCE[connectionId % COLORS_PRESENCE.length]}
+                            />
+                        );
+                    })}
+                </div>
+                <div className='mx-2'>
+                    <WhoIsHere
+                        userCount={others.length}
+                    />
+                </div>
+            </div>
+            <PageShow />
+        </div>
+    );
+}
+
+function PageShow() {
+    const shapes = useMap("shapes");
+
+    if (shapes === null || shapes === undefined) {
+        return (
+            <div className="loading">
+                Loading
+            </div>
+        );
+    } else {
+        return <Editor />;
+    }
+}
+
+function EditorWraped() {
+    return (
+        <RoomProvider id="react-todo-app" initialPresence={{ shapes: new LiveMap(), }} initialStorage={{ shapes: new LiveMap(), }}>
+            {/* <Suspense fallback={<div>Loading...</div>}> */}
+            <Room />
+            {/* </Suspense> */}
+        </RoomProvider>
+    );
+}
+
+
+export default EditorWraped
