649b6253f86eb94761f8cf783632c08446e4ea06
DreamWorker777 <110565432+DreamWorker777@users.noreply.github.com>
2022-09-27 15:25:02 -0400

    add google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,28 @@ import "./styles/tailwind.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import React from 'react';
+import ReactGA from 'react-ga'
 import { BrowserRouter as Router } from 'react-router-dom'
 import '@fontsource/inter/index.css'
 import "@fontsource/manrope";
 import './theme/global.scss'
+
+const GOOGLE_ANALYTICS_ID: string | undefined = 'UA-243172087-1'//process.env.REACT_APP_GOOGLE_ANALYTICS_ID
+if (typeof GOOGLE_ANALYTICS_ID === 'string') {
+    ReactGA.initialize(GOOGLE_ANALYTICS_ID)
+    ReactGA.set({
+        customBrowserType: `mobile`
+    })
+} else {
+    ReactGA.initialize('test', { testMode: true, debug: true })
+}
+
+window.addEventListener('error', error => {
+  ReactGA.exception({
+      description: `${error.message} @ ${error.filename}:${error.lineno}:${error.colno}`,
+      fatal: true
+  })
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
